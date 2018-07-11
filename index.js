96adbfda6e4515f8bd5d4a23280e80bc6d8df333
Maykonn <maykonn@outlook.com>
2018-07-11 13:21:19 -0300

    Refact to work with custom events

--- index.js
+++ index.js
@@ -8,12 +8,14 @@
 'use strict';
 
 import Time from "./src/Time";
+import CustomEvents from "./src/CustomEvents";
 
 ((window, undefined) => {
 
   // defining the namespace
   window.PageInfoJS = {
     Time: new Time(),
+    CustomEvents: new CustomEvents(),
   };
 
 })(window);
