1a9ef9660fcd5bb699f7ee58dd2d466f5021e7dd
Maykonn <maykonn@outlook.com>
2018-07-24 15:51:41 -0300

    New events for ready state changed

--- src/EventsList.js
+++ src/EventsList.js
@@ -1,7 +1,16 @@
 export default {
+  OnError: 'OnError',
   DOM: {
     ElementLoaded: 'DOMElementLoaded',
     ElementsLoadingPercentageIncremented: 'DOMElementsLoadingPercentageIncremented',
     AllElementsLoaded: 'DOMAllElementsLoadedEventName',
+  },
+  DocumentReadyStateChanged: {
+    Any: 'ReadyStateChanged',
+    ToUninitialized: 'Uninitialized',
+    ToLoading: 'Loading',
+    ToLoaded: 'Loaded',
+    ToInteractive: 'Interactive',
+    ToComplete: 'Complete',
   }
 }
